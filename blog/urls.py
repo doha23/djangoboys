fedb5f87281e0c8202428012553605cff225a6ee
doha23 <h1000541@naver.com>
2022-11-01 10:12:45 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-# 장고함수인 path와 blog 애플리케이션에서 사용할 모든 views를 import함
 from . import views
-
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+path('', views.post_list, name='post_list'),
+path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
